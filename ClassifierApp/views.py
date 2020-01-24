b58bf0948c26e59d263f1757da12f9575b80eb18
hieudepchai <hieunguyenvn98@gmail.com>
2020-01-24 17:45:30 +0700

    stable

--- ClassifierApp/views.py
+++ ClassifierApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 # from .utils import ImageCollector
-
+from selenium import webdriver
 # Create your views here.
 def index(request):
+    chromepath ='chromedriver.exe'
+    driver = webdriver.Chrome(chromepath)
+    driver.get('https://images.google.com/')
     return render(request, 'ClassifierApp/index.html', {'data': 1})
